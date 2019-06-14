960d1408054ad66f5f0ca8ab1e61aaa96135d94b
Michiel Schouten <51763911+ChielChiel@users.noreply.github.com>
2019-06-14 22:22:04 +0200

    Avaiability to write data to a .csv file
    Now there is an avaiability to use the INSERT sql statement to add data to a .csv file. 
    Works too with auto increment [ai] and timestamp [timestamp].

--- MyCSV/includes/Mycsv.php
+++ MyCSV/includes/Mycsv.php
@@ -81,24 +81,47 @@ class mycsv {
          }
         break;
       case "insert":
-       if(strtolower($prepSql[1]) == "into") {
-          $completePath = $this->completePath($prepSql[2]);
-          $fieldsToWrite = $this->get_string_between($this->sql, $prepSql[2], "VALUES");
-          $fieldsToWrite = explode(",",trim($this->get_string_between(trim(preg_replace('/\s+/', '', $fieldsToWrite)), "(", ")"), '"'));
-          $fields = $this->getLine($completePath, 0);
-           echo print_r($fields) . "<br>";
-         
-           echo print_r($fieldsToWrite) . "<br>";
-            
+        if(strtolower($prepSql[1]) == "into") {
+         //complete path to the .csv file or table
+         $completePath = $this->completePath($prepSql[2]);
+         //the fields extracted from the sql query to write to 
+         $fieldsToWrite = $this->get_string_between($this->sql, $prepSql[2], "VALUES");
+         $fieldsToWrite = explode(",",trim($this->get_string_between(trim(preg_replace('/\s+/', '', $fieldsToWrite)), "(", ")"), '"'));
+         //the avaiable fields extracted from the csv table 
+         $fields = $this->getLine($completePath, 0);
+         //the provided data to write extracted from the sql query 
+         $dataToWrite = $this->get_string_between($this->sql, "VALUES", ";");
+         $dataToWrite = explode(",",trim($this->get_string_between(trim(preg_replace('/\s+/', '', $dataToWrite)), "(", ")"), '"'));
+         //The difference between the provided columns to write and 
+         //the avaiable column extracted from the .csv file
          $dif=array_values(array_diff($fields,$fieldsToWrite));
-print_r($dif) . "<br>";
-         echo "wek" . $dif[0];
-         $nonExistent = array();
-         for($i=0; $i<count($dif); $i++) {
-          array_push($nonExistent, array_search($dif[$i], $fields)); 
+          
+         //puts every an template at the index where a computed value should be
+         foreach ($dif as $item) { 
+           array_splice($dataToWrite, array_search($item, $fields), 0,array('temp'));
          }
-           
-       }
+         //The final array to be written to the csv table
+         $final = array();
+         for($i=0; $i < count($fields); $i++) {
+          if (strpos($fields[$i], ' [ai]') !== false) { //auto increment
+           $cellData = $this->getCel($completePath, $this->lastLineNum($completePath), $fields[$i]);
+            if($cellData == $fields[$i]) { //no data in the .csv file yet. It is the table column. So start the [ai] at 1
+             $cellData = 1;
+           } else { //if there is already a number in place, add 1 to that one.
+              $cellData = ((int) preg_replace('/[^0-9]/', '', $cellData)) + 1;
+           }
+          } else if (strpos($fields[$i], ' [timestamp]') !== false) { //The column holds a timestamp so put the current time
+            $cellData = str_replace('"', "_",(string)date('d-m-Y H:i:s'));
+          } else { //just normal, remove the " and '  
+            $cellData = str_replace('"', "", str_replace("'", "",$dataToWrite[$i]));
+          } 
+          array_push($final, $cellData);
+         }
+          $fp = fopen($completePath, "a+");
+          fputcsv($fp, $final);
+          fclose($fp);
+        return true;  
+       } 
         break;
       case "select":
         echo "selecteer een waarde uit een database " . $prepSql[1];
